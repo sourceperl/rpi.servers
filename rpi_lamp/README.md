3c97fd75e4756cb8672a7dbe89142c5d7ddc7cf9
sourceperl <loic.celine@free.fr>
2016-09-01 19:01:28 +0200

    add rpi_lamp

---
+++ rpi_lamp/README.md
@@ -0,0 +1,13 @@
+# Server : basic LAMP server
+
+## What this server do
+
+- provide a web server (Apache2 with PHP5) with a database (MySQL)
+- also add phpadmin for manage database
+
+## Setup
+
+    # define some const like hostname or mysql root password
+    nano setup.sh
+    # launch setup
+    sudo setup.sh
---
+++ rpi_lamp/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# setup server script from a custom Raspbian image
+#
+# form source image: rpi_jessie_lite-xxxxxxxx-custom_fr.img
+
+# !!! define this before launch !!! 
+NEW_HOSTNAME="rpi_lamp"
+MYSQL_PWD="mysql"
+
+# vars
+NAME=$(basename $0)
+
+# check root
+[ $EUID -ne 0 ] && { printf "ERROR: $NAME needs to be run by root\n" 1>&2; exit 1; }
+
+# exit on error
+set -e
+
+# global env
+export DEBIAN_FRONTEND=noninteractive
+
+# define mysql params
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PWD"
+
+# define phpmyadmin params
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/mysql/admin-user string root"
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_PWD"
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_PWD"
+#debconf-set-selections <<<  "phpmyadmin phpmyadmin/mysql/app-pass password your-app-db-pwd"
+#debconf-set-selections <<<  "phpmyadmin phpmyadmin/app-password-confirm password your-app-pwd"
+
+# install packages
+apt-get install -y apache2 php5 mysql-server php5-mysql phpmyadmin
+
+# secure mysql
+mysql --defaults-file=/etc/mysql/debian.cnf <<< "DELETE FROM mysql.user WHERE User='';"
+mysql --defaults-file=/etc/mysql/debian.cnf <<< "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql --defaults-file=/etc/mysql/debian.cnf <<< "DROP DATABASE IF EXISTS test;"
+mysql --defaults-file=/etc/mysql/debian.cnf <<< "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+mysql --defaults-file=/etc/mysql/debian.cnf <<< "FLUSH PRIVILEGES;"
+
+# change the hostname
+CURRENT_HOSTNAME=$(cat /proc/sys/kernel/hostname)
+echo "$NEW_HOSTNAME" > /etc/hostname
+sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+
+# end messages
+echo "setup finish, take care to:"
+echo "-> make a reboot to update hostname"
+echo "-> add a network conf if don't use standard eth0 with DHCP"
+
